d3883a247559f32946a19b1cf51e5e540965150f
lokesh_krishna_cherukuri <lci2022015@iiitl.ac.in>
2023-10-09 14:11:36 +0530

    removed errors in the console

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,9 +21,6 @@ function Navbar() {
 {["Market","Exchange","Tutorials","Wallets"].map((item,index)=>(
     <NavbrItem key={index+item} title={item}/>
 ))}
-<li className='bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd] '>
-    Login
-</li>
 </ul>
 <div className="flex relative">
     {toggleMenu?<AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={()=>settoggleMenu(false)} />:<HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={()=>settoggleMenu(true)} />}
